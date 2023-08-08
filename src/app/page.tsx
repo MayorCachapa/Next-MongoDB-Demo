f103e767c96281882217bb19c4fd59f4e2edbf8e
Alejandro Rodriguez Hernandez <alejarod01@gmail.com>
2023-08-08 12:52:07 +0200

    Demo homepage partially created
    Added features:
    Created homepage main title
    Created app description.
    Language was originally destined to be in english. Had a change of heart and switched to Spanish instead

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,17 @@ type Props = {}
 
 export default function Home({}: Props) {
   return (
-    <div>page</div>
+    <section className="w-full flex-center flex-col">
+        <h1 className="head_text text-center">
+            Descubre y Comparte
+            <br className="max-md:hidden" />
+            <span className="green_gradient">Comandos energizados con IA</span>
+        </h1>
+        <p className="desc text-center">
+            Esta aplicación está diseñada con la intención de crear, compartir y descubrir comandos para IA (inteligencias artificiales). 
+            Es un proyecto abierto, para un mundo moderno y mas interactivo.
+        </p>
+
+    </section>
   )
 }

957f147b24adccd8eff50d43a9f3b9b85cf0df7c
Alejandro Rodriguez Hernandez <alejarod01@gmail.com>
2023-08-08 14:54:20 +0200

    NavBar finished for Mobile and Desktop
    Added features:
    Menu hides and shows depending on the screen of the user
    Menu on mobile gives option to log in and create a post
    No auth applied yet

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -6,10 +6,16 @@ import { useState, useEffect } from "react"
 import { signIn, signOut, useSession, getProviders, LiteralUnion, ClientSafeProvider } from 'next-auth/react'
 import { BuiltInProviderType } from "next-auth/providers/index"
 
-type Props = {}
-type ProvidersType = Record<LiteralUnion<BuiltInProviderType, string>, ClientSafeProvider> | null
+type Props = {};
+type ProvidersType = Record<LiteralUnion<BuiltInProviderType, string>, ClientSafeProvider> | null;
 
 export default function Nav({}: Props) {
+    const [toggleDropdown, setToggleDropdown] = useState(false);
+
+    function handleToggleDropdown() {
+        setToggleDropdown(false);
+    }
+    
     // const session = useSession();
     const user = true;
     const [providers, setProviders] = useState<ProvidersType>(null);
@@ -33,7 +39,7 @@ export default function Nav({}: Props) {
                 />
             </Link>
             <div className="sm:flex hidden">
-                { user !== null ? (
+                { user ? (
                     <div className="flex gap-3 md:gap-5">
                         <Link href="/create-prompt" className="slate_btn">
                             Crear Post
@@ -45,7 +51,7 @@ export default function Nav({}: Props) {
                             width={37}
                             height={37}
                             className="rounded-full" 
-                            alt="No picture provided" 
+                            alt="Profile Picture" 
                             />
                         </Link>
                     </div>
@@ -58,6 +64,50 @@ export default function Nav({}: Props) {
                         ))}
                     </>
                 ) }
+            </div>
+
+            {/* Mobile view: */}
+            <div className="sm:hidden flex relative">
+                { user ? (
+                    <div className="flex">
+                        <Image 
+                        src={Profile}
+                        alt="Profile Picture"
+                        width={37}
+                        height={37}
+                        className="rounded-full"
+                        // prev used instead because using the opposite value of the current state
+                        // (ie, !toggleDropdown) has caused unexpected behavior in the past
+                        onClick={() => setToggleDropdown( (prev) => !prev )}
+                        />
+                        
+                        {toggleDropdown && (
+                            <div className="dropdown">
+                                <Link href="/profile" className="dropdown_link" onClick={() => { handleToggleDropdown()}}>
+                                    Mi Perfil
+                                </Link>
+                                <Link href="/create-prompt" className="dropdown_link" onClick={() => { handleToggleDropdown()}}>
+                                    Crear Post
+                                </Link>
+                                <button
+                                type="button"
+                                onClick={() => {handleToggleDropdown(); signOut();}}
+                                className="mt-5 w-full slate_btn"
+                                >
+                                    Cerrar Sesión
+                                </button>
+                            </div>
+                        )}
+                    </div>
+                ) : (
+                    <>
+                        {providers && Object.values(providers).map((provider) => (
+                            <button className="slate_btn" type="button" key={provider.name} onClick={() => void signIn(provider.id)}>
+                                Abrir Sesión
+                            </button>
+                        ))}
+                    </>
+                )}
             </div>
         </nav>
       )
